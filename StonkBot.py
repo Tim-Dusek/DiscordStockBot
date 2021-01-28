35811afbecd298da98034297dbaecc2db33e3681
cdchris12 <chris.davis@amazee.io>
2021-01-28 01:59:06 -0600

    Testing image buffer for weeklygraph function #3

--- StonkBot.py
+++ StonkBot.py
@@ -261,7 +261,7 @@ async def weekgraph(ctx,company):
 	image_buffer.seek(0)
 	
 	# Push contents of image buffer to Discord
-	await ctx.send("week_graph.png", file=discord.File(image_buffer))
+	await ctx.send(file=discord.File(image_buffer, 'week_graph.png'))
 
 	# Close plot and image buffer
 	plt.close()
