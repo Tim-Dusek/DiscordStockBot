e5f8940e456607ff8d01c7e09d34a9bd6a063a23
cdchris12 <chris.davis@amazee.io>
2021-02-02 13:21:10 -0600

    Testing new grid system for crypto graphs #2

--- StonkBot.py
+++ StonkBot.py
@@ -97,9 +97,9 @@ async def create_crypto_graph(ctx, crypto: str, period: str, units: int) -> None
 		plt.plot(res_time, res_close)
 		plt.xlabel('Date and Time')
 		plt.ylabel('Price')
+		ax.grid = True
 		ax.xaxis_date()
 		#ax.format_xdata = mdates.DateFormatter('%b %d %H:%M') # Doesn't work??
-		ax.grid = True
 		fig.autofmt_xdate()
 
 		# Save image to buffer
